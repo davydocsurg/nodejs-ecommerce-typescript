d733b772a193b6c6fc8ddd0dc42885f33f2bda2f
davydocsurg <davydocsurg@gmail.com>
2022-08-03 11:15:06 +0100

    revamp model structure, add products data file

--- controllers/ProductController.ts
+++ controllers/ProductController.ts
@@ -1,7 +1,7 @@
 import { Product } from "../models/Product";
 
 export const getAddProductPage = (req: any, res: any, next: any) => {
-    res.render("add-product", {
+    res.render("admin/ add-product", {
         pageTitle: "Add Product",
         path: "/admin/add-product",
     });
@@ -14,11 +14,11 @@ export const createProduct = (req: any, res: any, next: any) => {
 };
 
 export const getProducts = (req: any, res: any, next: any) => {
-    const products = Product.fetchAll();
-
-    res.render("shop", {
-        prods: products,
-        pageTitle: "Shop",
-        path: "/",
+    Product.fetchAll((products: any) => {
+        res.render("shop/product-list", {
+            prods: products,
+            pageTitle: "Shop",
+            path: "/",
+        });
     });
 };
--- models/Product.ts
+++ models/Product.ts
@@ -1,21 +1,45 @@
+import fs from "fs";
+import path from "path";
+
 interface Products {
-    title: string;
+    title?: string;
 }
 
-const products: Products[] = [{ title: "" }];
+let products: Products[] = [{ title: "" }];
+
+const p = path.join(
+    path.dirname(process.mainModule?.filename),
+    "data",
+    "products.json"
+);
+
+const fetchProductsFromFile = (cb: Function) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+};
 
 export class Product {
-    title: string;
-    constructor(t: string) {
+    title?: string;
+    constructor(t?: string) {
         this.title = t;
     }
 
     save() {
-        products.push(this);
+        // products.push(this);
+        fetchProductsFromFile((products: [any]) => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err, "?");
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb: Function) {
+        fetchProductsFromFile(cb);
     }
 }
 
