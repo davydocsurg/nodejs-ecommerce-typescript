59db4b81be2c2bff6ebdc1776d0857f5eb0c1f9e
davydocsurg <davydocsurg@gmail.com>
2022-08-09 15:07:17 +0100

    revamp product and cart model, file path, controllers

--- models/Cart.ts
+++ models/Cart.ts
@@ -25,42 +25,42 @@ export class Cart {
         });
 
         // fetch the previous cart
-        // fs.readFile(PRODUCTS_JSON_PATH, (err: any, fileContent) => {
-        //     let cart = { products: [], totalPrice: 0 };
-        //     if (!err) {
-        //         cart = JSON.parse(fileContent.toString());
-        //     }
-        //     // console.log(cart, " aaaaa");
+        fs.readFile(PRODUCTS_JSON_PATH, (err: any, fileContent) => {
+            let cart = { products: [], totalPrice: 0 };
+            if (!err) {
+                cart = JSON.parse(fileContent.toString());
+            }
+            // console.log(cart, " aaaaa");
 
-        //     // analyze the cart => find existing product
-        //     if (cart.products) {
-        //         const existingProductsIndex = cart.products.findIndex(
-        //             (prod) => prod["id"] === id
-        //         );
-        //         const existingProducts: any =
-        //             cart.products[existingProductsIndex];
-        //         let updatedProducts: any;
-        //         console.log(existingProducts, " rrrrr");
+            // analyze the cart => find existing product
+            if (cart.products) {
+                const existingProductsIndex = cart.products.findIndex(
+                    (prod) => prod["id"] === id
+                );
+                const existingProducts: any =
+                    cart.products[existingProductsIndex];
+                let updatedProducts: any;
+                console.log(existingProducts, " rrrrr");
 
-        //         // add new product/increase quantity
-        //         if (existingProducts) {
-        //             updatedProducts = { ...existingProducts };
-        //             updatedProducts.qty += 1;
-        //             cart.products = [...cart.products];
-        //             cart.products[existingProductsIndex] = updatedProducts;
-        //         } else {
-        //             updatedProducts = { id: id, qty: 1 };
-        //             cart.products = [...cart.products, updatedProducts];
-        //         }
-        //         cart.totalPrice = cart.totalPrice + productPrice;
-        //         fs.writeFile(
-        //             PRODUCTS_JSON_PATH,
-        //             JSON.stringify(cart),
-        //             (err) => {
-        //                 console.log(err);
-        //             }
-        //         );
-        //     }
-        // });
+                // add new product/increase quantity
+                if (existingProducts) {
+                    updatedProducts = { ...existingProducts };
+                    updatedProducts.qty += 1;
+                    cart.products = [...cart.products];
+                    cart.products[existingProductsIndex] = updatedProducts;
+                } else {
+                    updatedProducts = { id: id, qty: 1 };
+                    cart.products = [...cart.products, updatedProducts];
+                }
+                cart.totalPrice = cart.totalPrice + productPrice;
+                fs.writeFile(
+                    PRODUCTS_JSON_PATH,
+                    JSON.stringify(cart),
+                    (err) => {
+                        console.log(err);
+                    }
+                );
+            }
+        });
     }
 }
