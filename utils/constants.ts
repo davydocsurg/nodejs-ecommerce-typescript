bf17d86409d98c7af1afd31e54f5da6d4231d375
davydocsurg <davydocsurg@gmail.com>
2022-08-25 18:28:37 +0100

    setup databse credentials

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const PRODUCTS_JSON_PATH = `${process.cwd()}/data/products.json`;
 export const CART_JSON_PATH = `${process.cwd()}/data/cart.json`;
+
+// database
+export const dbHost = process.env.DB_HOST;
+export const dbUser = process.env.DB_USER;
+export const db = process.env.DB_NAME;
+export const pwd = process.env.DB_PASSWORD;
