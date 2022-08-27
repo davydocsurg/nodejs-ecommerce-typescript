3e7a9c0e97c94e34c8501292f81c6d4347af5395
davydocsurg <davydocsurg@gmail.com>
2022-08-27 16:24:02 +0100

    setup product model with sequelize

--- models/Product.ts
+++ models/Product.ts
@@ -1,90 +1,154 @@
-import fs from "fs";
-import {
-    createProduct,
-    fetchProducts,
-    fetchProductById,
-    PRODUCTS_JSON_PATH,
-} from "../utils/constants";
-import { Cart } from "./Cart";
-const db = require("../utils/db");
+import { Model } from "sequelize";
+import { sequelize } from "../utils/db";
 
-export interface Products {
-    id?: string;
-    title?: string;
-    imageUrl?: string;
-    description?: string;
+interface ProductDetails {
+    id: string;
+    title: string;
+    imageUrl: string;
+    description: string;
     price: number;
 }
 
-// let products: Products[];
+module.exports = (DataTypes: any) => {
+    class Product extends Model<ProductDetails> implements ProductDetails {
+        id!: string;
+        title!: string;
+        imageUrl!: string;
+        description!: string;
+        price!: number;
+    }
 
-const fetchProductsFromFile = (cb: Function) => {
-    //console.log(p);
+    Product.init(
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+            },
 
-    fs.readFile(PRODUCTS_JSON_PATH, "utf8", (err, fileContent) => {
-        if (err) {
-            return cb([]);
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+
+            imageUrl: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+
+            description: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+
+            price: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            sequelize,
+            modelName: "Product",
         }
-        let data = JSON.parse(fileContent.toString());
-        // return data;
-        cb(data);
-    });
+    );
+
+    return Product;
 };
 
-export class Product {
-    id?: string;
-    title?: string;
-    imageUrl?: string;
-    description?: string;
-    price?: number;
+// const Product=sequelize.define('products',{
+//      id:{
+//         type:Sequelize.
+//      }
+// })
 
-    constructor(
-        // id?: string,
-        title?: string,
-        price?: number,
-        imageUrl?: string,
-        description?: string
-    ) {
-        // this.id = id;
-        this.title = title;
-        this.price = price;
-        this.imageUrl = imageUrl;
-        this.description = description;
-    }
+// import fs from "fs";
+// import {
+//     createProduct,
+//     fetchProducts,
+//     fetchProductById,
+//     PRODUCTS_JSON_PATH,
+// } from "../utils/constants";
+// import { Cart } from "./Cart";
+// const db = require("../utils/db");
 
-    save() {
-        return db.execute(createProduct, [
-            this.title,
-            this.price,
-            this.imageUrl,
-            this.description,
-        ]);
-    }
+// export interface Products {
+//     id?: string;
+//     title?: string;
+//     imageUrl?: string;
+//     description?: string;
+//     price: number;
+// }
 
-    static fetchAll() {
-        return db.execute(fetchProducts);
-    }
+// // let products: Products[];
 
-    static findById(id: any) {
-        return db.execute(fetchProductById, [id]);
-    }
+// const fetchProductsFromFile = (cb: Function) => {
+//     //console.log(p);
 
-    static deleteById(id: string) {
-        fetchProductsFromFile((products: Array<Products>) => {
-            const product = products.find((prod) => prod.id === id);
-            const updatedProducts = products.filter((prod) => prod.id !== id);
-            console.log("from deleteById");
+//     fs.readFile(PRODUCTS_JSON_PATH, "utf8", (err, fileContent) => {
+//         if (err) {
+//             return cb([]);
+//         }
+//         let data = JSON.parse(fileContent.toString());
+//         // return data;
+//         cb(data);
+//     });
+// };
 
-            fs.writeFile(
-                PRODUCTS_JSON_PATH,
-                JSON.stringify(updatedProducts),
-                (err) => {
-                    if (!err) {
-                        Cart.deleteProduct(id, product?.price);
-                    }
-                    console.error(err);
-                }
-            );
-        });
-    }
-}
+// export class Product {
+//     id?: string;
+//     title?: string;
+//     imageUrl?: string;
+//     description?: string;
+//     price?: number;
+
+//     constructor(
+//         // id?: string,
+//         title?: string,
+//         price?: number,
+//         imageUrl?: string,
+//         description?: string
+//     ) {
+//         // this.id = id;
+//         this.title = title;
+//         this.price = price;
+//         this.imageUrl = imageUrl;
+//         this.description = description;
+//     }
+
+//     save() {
+//         return db.execute(createProduct, [
+//             this.title,
+//             this.price,
+//             this.imageUrl,
+//             this.description,
+//         ]);
+//     }
+
+//     static fetchAll() {
+//         return db.execute(fetchProducts);
+//     }
+
+//     static findById(id: any) {
+//         return db.execute(fetchProductById, [id]);
+//     }
+
+//     static deleteById(id: string) {
+//         fetchProductsFromFile((products: Array<Products>) => {
+//             const product = products.find((prod) => prod.id === id);
+//             const updatedProducts = products.filter((prod) => prod.id !== id);
+//             console.log("from deleteById");
+
+//             fs.writeFile(
+//                 PRODUCTS_JSON_PATH,
+//                 JSON.stringify(updatedProducts),
+//                 (err) => {
+//                     if (!err) {
+//                         Cart.deleteProduct(id, product?.price);
+//                     }
+//                     console.error(err);
+//                 }
+//             );
+//         });
+//     }
+// }
