a4c23aa3b64c9588f925496089c260a7ee3904bc
davydocsurg <davydocsurg@gmail.com>
2022-10-18 03:20:02 +0100

    downgrade chalk to v4.1.2 to fix typescript compilation issues

--- helpers/logs.ts
+++ helpers/logs.ts
@@ -1,8 +1,17 @@
 import chalk from "chalk";
+// const importFresh = async (modulePath: any) =>
+//     import(`${modulePath}?x=${new Date()}`);
+// const chalk = (await importFresh("chalk")).default;
 
 export default class Logging {
     // public static log = (args:any)=> this.info(args);
     public static log = (args: any) => this.info(args);
+    public static success = (args: any) =>
+        console.log(
+            chalk.green(`[${new Date().toLocaleString()}][INFO]`),
+            typeof args === "string" ? chalk.greenBright(args) : args
+        );
+
     public static info = (args: any) =>
         console.log(
             chalk.blue(`[${new Date().toLocaleString()}][INFO]`),
--- utils/db.ts
+++ utils/db.ts
@@ -1,18 +1,15 @@
 import mongoose from "mongoose";
 import { localDB, mongoDbUrl } from "./constants";
-import chalk from "chalk";
 import Logging from "../helpers/logs";
 
 export const mongoDBConnection = async () => {
-    console.log("connecting...");
-    // Logging.info("connecting...");
+    Logging.info("connecting...");
 
     try {
         await mongoose.connect(localDB);
-        console.log(`Database connected successfully`);
-        // console.log(chalk.bgGreen("Database connected successfully"));
+        Logging.success(`Database connected successfully`);
     } catch (err) {
-        console.error(err);
-        console.error("Connection refused");
+        Logging.error(err);
+        Logging.error("Connection refused");
     }
 };
